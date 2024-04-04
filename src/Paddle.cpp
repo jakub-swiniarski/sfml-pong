b9ab6fa731c91c791dd9c48d7580b2304cd7a3e1
jakub-swiniarski <jakub-swiniarski@protonmail.com>
2024-04-04 20:21:51 +0200

    observer

--- src/Paddle.cpp
+++ src/Paddle.cpp
@@ -28,8 +28,8 @@ bool Paddle::ball_collision_check(Ball &ball) {
     if (ball.getPosition().y <= getPosition().y + getSize().y && ball.getPosition().y + ball.getRadius() >= getPosition().y) {
         if (ball.getPosition().x + ball.getRadius() >= getPosition().x
         && ball.getPosition().x <= getPosition().x + getSize().x) {
-            ball.speed_x *= -1;
-            ball.speed_x += (ball.speed_x > 0) ? 50 : -50;
+            //ball.speed_x *= -1;
+            //ball.speed_x += (ball.speed_x > 0) ? 50 : -50;
             return 1;
         }
     }
