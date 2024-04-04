17cd4cd23dc16445e16a0cc2f5842959294d9147
jakub-swiniarski <jakub-swiniarski@protonmail.com>
2024-04-04 20:08:14 +0200

    ball update func

--- src/Ball.cpp
+++ src/Ball.cpp
@@ -5,10 +5,14 @@
 #include "config.hpp"
 
 Ball::Ball(float x, float y) {
-    setRadius(10.f);
+    setRadius(10.f); /* TODO: make this configurable */
     setFillColor(BALL_COLOR);
     setPosition(x, y);
 
     speed_x = BALL_START_SPEED_X;
     speed_y = BALL_START_SPEED_Y;
+}
+
+void Ball::update(float mod) {
+    move(speed_x * mod, speed_y * mod);
 }
