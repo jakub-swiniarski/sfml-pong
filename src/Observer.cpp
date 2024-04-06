c8658a7f4597aab00c37c6e663dc8d6b4c8a9c15
jakub-swiniarski <jakub-swiniarski@protonmail.com>
2024-04-06 10:15:41 +0200

    observer manages points

--- src/Observer.cpp
+++ src/Observer.cpp
@@ -4,6 +4,8 @@
 #include "Observer.hpp"
 #include "Paddle.hpp"
 
+#include "config.hpp"
+
 Observer::Observer(Ball *b, Paddle *p, Paddle *e) {
     ball = b;
     player = p;
@@ -12,7 +14,14 @@ Observer::Observer(Ball *b, Paddle *p, Paddle *e) {
 
 void Observer::update(void) {
     if (player->ball_collision_check(ball)
-    || enemy->ball_collision_check(ball)) {
+    || enemy->ball_collision_check(ball))
         ball->bounce_paddle();
+
+    if (ball->getPosition().x <= 0.f) {
+        player->update_counter();
+        ball->reset();
+    } else if (ball->getPosition().x >= SCREEN_WIDTH - ball->getRadius()) {
+        enemy->update_counter();
+        ball->reset();
     }
 }
