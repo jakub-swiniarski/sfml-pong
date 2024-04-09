7805608666aee0a83045af40ec3c0a1ddd6edc2d
Jakub Świniarski <77209709+jakub-swiniarski@users.noreply.github.com>
2024-04-09 18:27:15 +0200

    shorter collision detection

--- src/Paddle.cpp
+++ src/Paddle.cpp
@@ -19,15 +19,7 @@ Paddle::Paddle(float x, float y, sf::Color p_col, sf::Color t_col) {
 }
 
 bool Paddle::ball_collision_check(Ball *b) {
-    if (!getGlobalBounds().intersects(b->getGlobalBounds()))
-        return 0;
-
-    if (getGlobalBounds().contains(b->getPosition().x + b->getRadius() / 2.f, b->getPosition().y)
-    || getGlobalBounds().contains(b->getPosition().x + b->getRadius() / 2.f, b->getPosition().y + b->getRadius())) {
-        b->bounce_y();
-        return 1;
-    } else if (getGlobalBounds().contains(b->getPosition().x, b->getPosition().y + b->getRadius() / 2.f)
-    || getGlobalBounds().contains(b->getPosition().x + b->getRadius(), b->getPosition().y + b->getRadius() / 2.f)) {
+    if (getGlobalBounds().intersects(b->getGlobalBounds())) {
         b->bounce_x();
         return 1;
     } 
