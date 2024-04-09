0272ada6b83f32b8dc1be57403311d96cbca956a
jakub-swiniarski <jakub-swiniarski@protonmail.com>
2024-04-09 21:34:47 +0200

    reset path

--- src/main.cpp
+++ src/main.cpp
@@ -27,7 +27,7 @@ static sf::SoundBuffer pop_buffer;
 static sf::RenderWindow window(sf::VideoMode(SCREEN_WIDTH, SCREEN_HEIGHT), "Pong", sf::Style::None);
 
 /* constants */
-static const std::string FILEPATH = "res/"; /* TODO: reset this before merging */
+static const std::string FILEPATH = "/usr/local/share/pong/";
 
 /* function implementations */
 void draw(void) {
