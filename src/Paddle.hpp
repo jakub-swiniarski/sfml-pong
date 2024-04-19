9f99e8b00c5584b554f3dd09de3af042dc774ec7
Jakub Świniarski <77209709+jakub-swiniarski@users.noreply.github.com>
2024-04-19 19:00:50 +0200

    private counter

--- src/Paddle.hpp
+++ src/Paddle.hpp
@@ -8,14 +8,14 @@
 class Ball;
 
 class Paddle : public sf::RectangleShape {
+    sf::Text counter;
     int score;
 
 public:
-    sf::Text counter;
-
     Paddle(float x, float y, sf::Color p_col, sf::Color t_col);
     bool ball_collision_check(Ball *b) const;
     void border_check(void);
+    sf::Text &get_counter(void);
     void update_counter(void);
 };
 
