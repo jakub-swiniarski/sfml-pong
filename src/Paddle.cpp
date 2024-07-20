c762dfc9b04045b32cebd825dae6932627284baf
jakub-swiniarski <jakub-swiniarski@protonmail.com>
2024-07-20 13:27:28 +0200

    style changes

--- src/Paddle.cpp
+++ src/Paddle.cpp
@@ -15,7 +15,7 @@ Paddle::Paddle(float x, float y, sf::Color p_col, sf::Color t_col)
     counter.setString("0");
     counter.setCharacterSize(cfg::counter::font_size);
     counter.setFillColor(t_col);
-    counter.setPosition(0.f, 0.f);
+    counter.setPosition(0.0f, 0.0f);
 }
 
 bool Paddle::ball_collision_check(Ball& ball) const {
@@ -25,14 +25,14 @@ bool Paddle::ball_collision_check(Ball& ball) const {
     if (getGlobalBounds().contains(ball.getPosition().x, ball.getPosition().y + ball.getRadius())) { // left side of the ball
         ball.setPosition(getPosition().x + getSize().x, ball.getPosition().y); // we move the ball to prevent it from getting stuck
         ball.bounce_x();
-    } else if (getGlobalBounds().contains(ball.getPosition().x + 2.f * ball.getRadius(), ball.getPosition().y + ball.getRadius())) { // right side of the ball
-        ball.setPosition(getPosition().x - 2.f * ball.getRadius(), ball.getPosition().y);
+    } else if (getGlobalBounds().contains(ball.getPosition().x + 2.0f * ball.getRadius(), ball.getPosition().y + ball.getRadius())) { // right side of the ball
+        ball.setPosition(getPosition().x - 2.0f * ball.getRadius(), ball.getPosition().y);
         ball.bounce_x();
     } else if (getGlobalBounds().contains(ball.getPosition().x + ball.getRadius(), ball.getPosition().y)) { // top of the ball
         ball.setPosition(ball.getPosition().x, getPosition().y + getSize().y);
         ball.bounce_y();
-    } else if (getGlobalBounds().contains(ball.getPosition().x + ball.getRadius(), ball.getPosition().y + 2.f * ball.getRadius())) { // bottom of the ball
-        ball.setPosition(ball.getPosition().x, getPosition().y - 2.f * ball.getRadius());
+    } else if (getGlobalBounds().contains(ball.getPosition().x + ball.getRadius(), ball.getPosition().y + 2.0f * ball.getRadius())) { // bottom of the ball
+        ball.setPosition(ball.getPosition().x, getPosition().y - 2.0f * ball.getRadius());
         ball.bounce_y();
     }
 
@@ -40,7 +40,7 @@ bool Paddle::ball_collision_check(Ball& ball) const {
 }
 
 void Paddle::border_check(void) {
-    setPosition(getPosition().x, std::clamp(getPosition().y, 0.f, cfg::window::height - getSize().y));
+    setPosition(getPosition().x, std::clamp(getPosition().y, 0.0f, cfg::window::height - getSize().y));
 }
 
 sf::Text& Paddle::get_counter(void) {
--- src/config.hpp
+++ src/config.hpp
@@ -5,39 +5,39 @@
 
 namespace cfg {
     namespace window {
-        static constexpr unsigned int width = 1280;
+        static constexpr unsigned int width  = 1280;
         static constexpr unsigned int height = 720;
-        static constexpr unsigned int fps = 60;
+        static constexpr unsigned int fps    = 60;
     }
 
     namespace counter {
         static constexpr unsigned int font_size = 120;
-        static constexpr float shift_x = 300.f;
+        static constexpr float shift_x          = 300.0f;
     }
 
     namespace ball {
         static const sf::Color color(255, 255, 255);
-        static constexpr float radius = 10.f;
-        static constexpr float speed_boost = 50.f;
-        static constexpr float speed_x = 200.f;
-        static constexpr float speed_y = 220.f;
+        static constexpr float radius      = 10.0f;
+        static constexpr float speed_boost = 50.0f;
+        static constexpr float speed_x     = 200.0f;
+        static constexpr float speed_y     = 220.0f;
     }
 
     namespace paddle {
-        static constexpr float width = 20.f;
-        static constexpr float height = 100.f;
+        static constexpr float width  = 20.0f;
+        static constexpr float height = 100.0f;
     }
 
     namespace enemy {
         static const sf::Color color(255, 0, 0);
         static const sf::Color counter_color(255, 170, 170);
-        static constexpr float speed_y = 200.f;
+        static constexpr float speed_y = 200.0f;
     }
 
     namespace player {
         static const sf::Color color(0, 255, 0);
         static const sf::Color counter_color(170, 255, 170);
-        static constexpr float speed_y = 200.f;
+        static constexpr float speed_y = 200.0f;
     }
 }
 
